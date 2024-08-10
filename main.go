e5a9224cc11302b418112c31b2e916a4e9b8f48c
unknown <fepinzon77@gmail.com>
2024-08-09 23:07:12 -0500

    [FIX] Now with mongodb

--- main.go
+++ main.go
@@ -1,86 +1,93 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type Todo struct{
-	ID int `json:"id"`
+type Todo struct {
+	ID	int `json:"_id" bson:"_id"`
 	Completed bool `json:"completed"`
 	Body	string `json:"body"`
-}
+} 
 
-//Have in mind that this & is refering to a memory space of something.
-//And this * is a pointer that help u to have the value that is on that memory space
 
-func main() {
-	fmt.Println("Hello World's")
-	app := fiber.New()
+var collection *mongo.Collection
+
+func main()  {
+	fmt.Println("Started")
 
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal("Error loading .env file:",err)
 	}
 
-	PORT := os.Getenv("PORT")
+	MONGODB_URI := os.Getenv("MONGODB_URI")
+	clientOptions := options.Client().ApplyURI(MONGODB_URI)
+	client,err := mongo.Connect(context.Background(),clientOptions)
 
-	todos := []Todo{}
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	app.Get("/api/todos", func(c *fiber.Ctx) error {
-		return c.Status(200).JSON(todos)
-	})
+	defer client.Disconnect(context.Background())
 
-	//Create task
-	app.Post("/api/todos", func(c *fiber.Ctx) error {
-		todo := &Todo{} //for todo is asign the space memory of Todo
+	err = client.Ping(context.Background(),nil)
 
-		if err := c.BodyParser(todo); err != nil {
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to DB")
+
+	collection = (client.Database("godb").Collection("todos"))
+
+	app := fiber.New()
+
+	app.Get("/api/todos", getTodos)
+	// app.Post("/api/todos", createTodo)
+	// app.Patch("/api/todos/:id", updateTodo)
+	// app.Delete("/api/todos/:id", deleteTodo)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
+
+	if os.Getenv("ENV") == "production" {
+		app.Static("/", "./client/dist")
+	}
+
+	log.Fatal(app.Listen("0.0.0.0:" + port))
+
+}
+
+func getTodos(c *fiber.Ctx) error {
+	var todos []Todo
+
+	cursor, err := collection.Find(context.Background(), bson.M{})
+
+	if err != nil {
+		return err
+	}
+
+	defer cursor.Close(context.Background())
+
+	for cursor.Next(context.Background()) {
+		var todo Todo
+		if err := cursor.Decode(&todo); err != nil {
 			return err
 		}
+		todos = append(todos, todo)
+	}
 
-		if todo.Body == "" {
-			return c.Status(400).JSON(fiber.Map{"error": "Todo body is required"})
-		}
-
-		todo.ID = len(todos) + 1
-		todos = append(todos, *todo) // Here u call for the value that is on that memory space
-
-		return c.Status(201).JSON(todo)
-	})
-
-	//Update task
-	app.Patch("/api/todos/:id", func(c *fiber.Ctx) error {
-		id := c.Params("id")
-
-		for i, todo := range todos{
-			if fmt.Sprint(todo.ID) == id {
-				todos[i].Completed = true
-				return c.Status(200).JSON(todos[i])
-			}
-		}
-
-		return c.Status(404).JSON(fiber.Map{"error" : "Task not found"})
-	})
-
-	app.Delete("/api/todos/:id", func(c *fiber.Ctx) error {
-		id := c.Params("id")
-
-		for i, todo := range todos {
-			if fmt.Sprint(todo.ID) == id {
-				todos = append(todos[:i], todos[i+1:]...)
-				return c.Status(200).JSON(fiber.Map{"success" : true})
-			}
-		}
-
-		return c.Status(404).JSON(fiber.Map{"error" : "Task not found"})
-
-	})
-
-	app.Listen(":" + PORT)
-
+	return c.JSON(todos)
 }
